c3767c576908d14a873ec3d2d118481c2a3d4a12
Cameron mitchell Shaw <845872712@ken20-202-12.local>
2017-11-30 19:57:00 +1300

    css animations

---
+++ TRAVL/js/custom.js
@@ -0,0 +1,10 @@
+
+// Mapbox controls 
+mapboxgl.accessToken = 'pk.eyJ1IjoiY2FtbXkxMSIsImEiOiJjamFib2p6cGwwMjNhMnhtaGptbmF1azJrIn0.OtcZ5rIhMtthxs7yu6KqmQ';
+var map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/cammy11/cjalqspmrczga2smswy6q4kgx',
+center: [169.130632, -41.709066],
+zoom: 5,
+});
+
