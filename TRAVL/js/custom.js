d7777078e836377bfa329442f783a67205adde5f
Cameron mitchell Shaw <845872712@ken20-202-12.local>
2017-12-05 18:59:24 +1300

    forms

--- TRAVL/js/custom.js
+++ TRAVL/js/custom.js
@@ -1,4 +1,8 @@
+
 var init = function () {
+
+		// document.getElementById("add").addEventListener("click", incrementValue);
+	function myForm() {
 
 		// Mapbox controls 
 		mapboxgl.accessToken = 'pk.eyJ1IjoiY2FtbXkxMSIsImEiOiJjamFib2p6cGwwMjNhMnhtaGptbmF1azJrIn0.OtcZ5rIhMtthxs7yu6KqmQ';
@@ -22,25 +26,50 @@ var init = function () {
 
 		// document.getElementById("subtract").addEventListener("click", down);
 
-		// document.getElementById("add").addEventListener("click", up);
+		
 
-		var btn = document.getElementsByClassName('button-primary');
-		btn[0].addEventListener('click', up('10'), false);
+		// var btn = document.getElementsByClassName('button-primary');
+		// btn[0].addEventListener('click', up('10'), false);
 
-		function up(max) {
-		    document.getElementById("myNumber").value = parseInt(document.getElementById("myNumber").value) + 1;
-		    if (document.getElementById("myNumber").value >= parseInt(max)) {
-		        document.getElementById("myNumber").value = max;
-		    }
-		}
+		// function up(max) {
+		//     document.getElementById("myNumber").value = parseInt(document.getElementById("myNumber").value) + 1;
+		//     if (document.getElementById("myNumber").value >= parseInt(max)) {
+		//         document.getElementById("myNumber").value = max;
+		//     }
+		// }
 
-		up();
-		function down(min) {
-		    document.getElementById("myNumber").value = parseInt(document.getElementById("myNumber").value) - 1;
-		    if (document.getElementById("myNumber").value <= parseInt(min)) {
-		        document.getElementById("myNumber").value = min;
-		    }
-		}
-		down();
+		// up();
+		// function down(min) {
+		//     document.getElementById("myNumber").value = parseInt(document.getElementById("myNumber").value) - 1;
+		//     if (document.getElementById("myNumber").value <= parseInt(min)) {
+		//         document.getElementById("myNumber").value = min;
+		//     }
+		// }
+		// down();
+		// var btn = document.getElementsByClassName('button-primary');
+		// btn[0].addEventListener('click', addRemoveNumber, false);
 
-}();
+		function addRemoveNumber () {
+
+			function incrementValue() {		
+				var value = parseInt(document.getElementById('my-number').value, 10);
+				value = isNaN(value) ? 0 : value;
+				value++;
+				document.getElementById('my-number').value = value;
+
+			}
+			incrementValue();
+
+			function incrementValueDays() {
+				var value = parseInt(document.getElementById('my-number-days').value, 10);
+				value = isNaN(value) ? 0 : value;
+				value++;
+				document.getElementById('my-number-days').value = value;
+			}
+			incrementValueDays();
+		};
+
+	}; // myForm function ends
+	myForm ();
+
+}(); //main function ends
