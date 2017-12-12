e682f429cd44d0187f42ecb9c886d39cff5b3c90
Cameron mitchell Shaw <845872712@ken20-202-12.local>
2017-12-12 16:17:11 +1300

    map tidying

--- TRAVL/js/custom.js
+++ TRAVL/js/custom.js
@@ -1,126 +1,6 @@
 
 var init = function () {
 
-	function myForm() {
-
-
-		// Mapbox controls 
-		mapboxgl.accessToken = 'pk.eyJ1IjoiY2FtbXkxMSIsImEiOiJjamFib2p6cGwwMjNhMnhtaGptbmF1azJrIn0.OtcZ5rIhMtthxs7yu6KqmQ';
-
-		   	var directions = new MapboxDirections({
-	      		accessToken: 'pk.eyJ1IjoidmVyYXRlY2giLCJhIjoiY2phYmZ1NXFmMHIwMDM1cGV4bHV4bHhzbSJ9.gbT5J_uXxbjRRuj00D7Xeg',
-	      		unit: 'metric'
-    	});
-
-		var map = new mapboxgl.Map({
-			container: 'map',
-			style: 'mapbox://styles/cammy11/cjalqspmrczga2smswy6q4kgx',
-			center: [174.763817,-41.295241],
-			zoom: 12,
-			minZoom: 5
-		});
-
-		map.addControl(directions, 'top-left');
-
-    	console.log(directions);
-
-    	directions.on('route', function(directions){
-
-        	// var datapls = directions.getDestination();
-        	console.log(directions);
-        	console.log('Your trip will be: ' + directions.route['0'].distance / 1000 + ' kms');
-        	overDistanceCost.textContent = "FUEL:" + " " + ((directions.route[0].distance / 1000) * 9.7 / 100).toFixed(1) + "L";
-
-    	});  
-
-    	document.getElementById('submit-arrow').addEventListener('click', setRouteDynamically, false);
-
-	    function setRouteDynamically(){
-			var myLocation 			= document.getElementById('input-space-from').value;
-			var myDestinationIs 	= document.getElementById('input-space-to').value;
-	      	directions.setOrigin(myLocation); 
-	      	directions.setDestination(myDestinationIs);        
-	    }
-
-
-		// tipr plugin for tooltips
-		$(document).ready(function() {
-	     	$('.tip').tipr({
-	          	'speed': 400,
-          		'mode': 'above',
-          		'space': 7
-	     	});
-		});
-
-
-
-
-
-
-		/////////// modal pop out ///////////
-
-		// Get the modal
-		var modal 			= document.getElementById('myModal');
-
-		// Get the button that opens the modal
-		var btn 			= document.getElementById("motorbike");
-		var smartCarBtn 	= document.getElementById('smartCar');
-		var largeCarBtn 	= document.getElementById('largeCar');
-		var motorhomeBtn 	= document.getElementById('motorhome');
-		var h1Elem			= document.getElementsByClassName('head-modal');
-		var h2Elem			= document.getElementsByClassName('modalPrice');
-		var h2Elem2			= document.getElementsByClassName('modalPrice2');
-
-		// Get the <span> element that closes the modal
-		var span 			= document.getElementsByClassName("close")[0];
-
-		var buttons = document.getElementsByClassName('car');
-			for (var i = 0, len = buttons.length; i < len; i++) {
-    			buttons[i].onclick = function (){
-        			modal.style.display = "block";
-			};
-		}
-
-		// When the user clicks on <span> (x), close the modal
-		span.onclick = function() {
-		    modal.style.display = "none";
-		};
-
-		// When the user clicks anywhere outside of the modal, close it
-		window.onclick = function(event) {
-		    if (event.target == modal) {
-		        modal.style.display = "none";
-		    }
-		};
-
-		smartCarBtn.addEventListener('click', function() {
-			document.getElementById('photoTransport').src="img/smartcar.png";
-			h1Elem["0"].textContent = "SMARTCAR";
-			h2Elem["0"].textContent = "$129 PER DAY";
-			h2Elem2["0"].textContent ="8.5L/100KM";
-		});
-
-		largeCarBtn.addEventListener('click', function() {
-			document.getElementById('photoTransport').src="img/large-car.png";
-			h1Elem["0"].textContent = "CHRYSLER 200";
-			h2Elem["0"].textContent = "$144 PER DAY";
-			h2Elem2["0"].textContent ="9.7L/100KM";
-		});
-
-		motorhomeBtn.addEventListener('click', function() {
-			document.getElementById('photoTransport').src="img/motorhome.png";
-			h1Elem["0"].textContent = "MOTORHOME";
-			h2Elem["0"].textContent = "$200 PER DAY";
-			h2Elem2["0"].textContent ="17L/100KM";
-		});
-
-		btn.addEventListener('click', function() {
-			document.getElementById('photoTransport').src="img/motorbike.png";
-			h1Elem["0"].textContent = "MOTORBIKE";
-			h2Elem["0"].textContent = "$109 PER DAY";
-			h2Elem2["0"].textContent ="3.7L/100KM";
-		});
-
  		//////////// input value variables //////////////////////////////////
 		var peopleAmount 		= document.getElementById('my-number');
 		var daysAmount 			= document.getElementById('my-number-days');
@@ -144,16 +24,149 @@ var init = function () {
 		//////////////////// cost calculating variables //////////////////////////////////
 		var fuelCost 			= document.getElementById('fuel-cost');
 		var overDistanceCost 	= document.getElementById('over-cost');
+		var submitArrow		 	= document.getElementById('submit-arrow');
+		/////////////////// input form values ////////////////////////////////////////////
+		var myLocation 			= document.getElementById('input-space-from').value;
+		var myDestinationIs 	= document.getElementById('input-space-to').value;
+		// Get the modal
+		var modal 				= document.getElementById('myModal');
+		// Get the button that opens the modal
+		var btn 				= document.getElementById("motorbike");
+		var smartCarBtn 		= document.getElementById('smartCar');
+		var largeCarBtn 		= document.getElementById('largeCar');
+		var motorhomeBtn 		= document.getElementById('motorhome');
+		/// change inner content in modal variables
+		var modalImage			= document.getElementById('photoTransport');	
+		var h1Elem				= document.getElementsByClassName('head-modal');
+		var h2Elem				= document.getElementsByClassName('modalPrice');
+		var h2Elem2				= document.getElementsByClassName('modalPrice2');
+		// Get the <span> element that closes the modal
+		var span 			= document.getElementsByClassName("close")[0];
+
+	function myForm() {
+
+
+		////// click events /////
+		addPeople.addEventListener('click', increaseValue, false);
+		subtractPeople.addEventListener('click', decreaseValue, false);
+		addDays.addEventListener('click', increaseValue2, false);
+		subtractDays.addEventListener('click', decreaseValue2, false);
+		submitArrow.addEventListener('click', function() {
+
+     		setRouteDynamically();
+     		carChooser();
+
+		});
+
+		// Mapbox controls 
+		mapboxgl.accessToken = 'pk.eyJ1IjoiY2FtbXkxMSIsImEiOiJjamFib2p6cGwwMjNhMnhtaGptbmF1azJrIn0.OtcZ5rIhMtthxs7yu6KqmQ';
+		   	var directions = new MapboxDirections({
+	      		accessToken: 'pk.eyJ1IjoidmVyYXRlY2giLCJhIjoiY2phYmZ1NXFmMHIwMDM1cGV4bHV4bHhzbSJ9.gbT5J_uXxbjRRuj00D7Xeg',
+	      		unit: 'metric'
+    	});
+
+		var map = new mapboxgl.Map({
+			container: 'map',
+			style: 'mapbox://styles/cammy11/cjalqspmrczga2smswy6q4kgx',
+			center: [174.763817,-41.295241],
+			zoom: 12,
+			minZoom: 5
+		});
+
+		map.addControl(directions, 'top-left');
+
+    	console.log(directions);
+
+    	directions.on('route', function(directions){
+
+        	console.log(directions);
+        	console.log('Your trip will be: ' + directions.route['0'].distance / 1000 + ' kms');
+        	overDistanceCost.textContent = "FUEL:" + " " + ((directions.route[0].distance / 1000) * 9.7 / 100).toFixed(1) + "L";
+
+    	});  
+
+	    function setRouteDynamically(){
+							
+			var myLocation 			= document.getElementById('input-space-from').value;
+			var myDestinationIs 	= document.getElementById('input-space-to').value;
+
+      		directions.setOrigin(myLocation); 
+      		directions.setDestination(myDestinationIs);        
+	    }	
+
+
+		// tipr plugin for tooltips
+		$(document).ready(function() {
+	     	$('.tip').tipr({
+	          	'speed': 400,
+          		'mode': 'above',
+          		'space': 7
+	     	});
+		});
+
+		/////////// modal pop out //////////
+
+		// on click of ANY of the cars, display the modal 
+		var buttons = document.getElementsByClassName('car');
+			for (var i = 0, len = buttons.length; i < len; i++) {
+    			buttons[i].onclick = function (){
+        			modal.style.display = "block";
+			};
+		}
+
+		// When the user clicks on <span> (x), close the modal
+		span.onclick = function() {
+		    modal.style.display = "none";
+		};
+
+		// When the user clicks anywhere outside of the modal, close it
+		window.onclick = function(event) {
+		    if (event.target == modal) {
+		        modal.style.display = "none";
+		    }
+		};
+
+		smartCarBtn.addEventListener('click', function() {
+			modalImage.src="img/smartcar.png";
+			h1Elem["0"].textContent = "SMARTCAR";
+			h2Elem["0"].textContent = "$129 PER DAY";
+			h2Elem2["0"].textContent ="8.5L/100KM";
+		});
+
+		largeCarBtn.addEventListener('click', function() {
+			modalImage.src="img/large-car.png";
+			h1Elem["0"].textContent = "CHRYSLER 200";
+			h2Elem["0"].textContent = "$144 PER DAY";
+			h2Elem2["0"].textContent ="9.7L/100KM";
+		});
+
+		motorhomeBtn.addEventListener('click', function() {
+			modalImage.src="img/motorhome.png";
+			h1Elem["0"].textContent = "MOTORHOME";
+			h2Elem["0"].textContent = "$200 PER DAY";
+			h2Elem2["0"].textContent ="17L/100KM";
+		});
+
+		btn.addEventListener('click', function() {
+			modalImage.src="img/motorbike.png";
+			h1Elem["0"].textContent = "MOTORBIKE";
+			h2Elem["0"].textContent = "$109 PER DAY";
+			h2Elem2["0"].textContent ="3.7L/100KM";
+		});
 		
 
+		function carChooser () {
 
+			event.preventDefault();
 
+				var pricePerDay = {
 
-		$(document).ready(function(){
+					motorbike: 109,
+					smallCar: 129,
+					largeCar: 144,
+					motorhome: 200
 
-    		$("#submit-arrow").click(function(event){
-    			event.preventDefault();
-
+				};
 
 					var peopleAmountInt = parseInt(peopleAmount.value);
 					var daysAmountInt 	= parseInt(daysAmount.value);
@@ -186,15 +199,6 @@ var init = function () {
 					}
 
 
-				var pricePerDay = {
-
-					motorbike: 109,
-					smallCar: 129,
-					largeCar: 144,
-					motorhome: 200
-
-				};
-
 				$(carSelect).change(function() {
 
 		    		if (carSelectMotor.checked == true) {
@@ -205,26 +209,14 @@ var init = function () {
 		        			fuelCost.textContent = "HIRE:" + " " + "$" + daysAmountInt * pricePerDay.largeCar;
 		        	} else if (carSelectMotorhome.checked == true) {
 		        			fuelCost.textContent = "HIRE:" + " " + "$" + daysAmountInt * pricePerDay.motorhome;
-		        			// overDistanceCost.textContent = "OVER:" + " " + 8.9 * directions.route[0].distance / 1000 / 100;
 		        	}
 
 				});
+	 	}
 
-				// overDistanceCost.textContent = "OVER:" + " " + 8.9 * directions.route['0'].distance / 1000 / 100; 
 
-				// 214.9 = petrol price cents
+		////////////  button increment and decrement  //////////////
 
-        		// console.log(daysAmount.value);
-        		// // console.log(carSelect[0].checked);
-        		// console.log(carSelectSmall.checked);
-        		// console.log(carSelectLarge.checked);
-        		// console.log(carSelectMotorhome.checked);
-   		 	});
-		});
-
-		////////////  button increment and decrement  ////////////////
-
-		addPeople.addEventListener('click', increaseValue);
 		function increaseValue() {
 			  var value = parseInt(peopleAmount.value, 10);
 			  value = isNaN(value) ? 0 : value;
@@ -232,7 +224,6 @@ var init = function () {
 			  peopleAmount.value = value;
 		}
 
-		subtractPeople.addEventListener('click', decreaseValue);
 		function decreaseValue() {
 			  var value = parseInt(peopleAmount.value, 10);
 			  value = isNaN(value) ? 0 : value;
@@ -240,8 +231,7 @@ var init = function () {
 			  value--;
 			  peopleAmount.value = value;
 		}
-		
-		addDays.addEventListener('click', increaseValue2);
+
 		function increaseValue2() {
 			  var value = parseInt(daysAmount.value, 10);
 			  value = isNaN(value) ? 0 : value;
@@ -249,7 +239,6 @@ var init = function () {
 			  daysAmount.value = value;
 		}
 
-		subtractDays.addEventListener('click', decreaseValue2);
 		function decreaseValue2() {
 			  var value = parseInt(daysAmount.value, 10);
 			  value = isNaN(value) ? 0 : value;
